e213a3dbc72d3212ed532e7f354112f4cbc65b88
Aquiles Carattino <aquiles@uetke.com>
2019-12-20 13:30:54 +0100

    Updated newsletter subscription to uetke instead of elastic mail

--- template/uetke/static/js/newsletter_subscribe.js
+++ template/uetke/static/js/newsletter_subscribe.js
@@ -3,17 +3,12 @@ $(document).ready(function () {
     event.preventDefault();
 
     var email = $("#email").val();
-    $.ajax({
+        $.ajax({
         type: "POST",
-        url: "https://api.elasticemail.com/v2/contact/add",
-        data: {
-          'publicAccountID': '7616a022-4e37-40ba-b90e-6c3735d8440e',
-          'email': email,
-          'listName': 'Uetke Newsletter',
-          'activationReturnUrl': 'https://www.uetke.com',
-          'activationTemplate': '',
-          'source': 'WebForm',
-          'ewf_captcha': 'false'},
+        dataType: 'json',
+        contentType: 'application/json',
+        url: "https://uetke.uetke.com/subscribe-newsletter",
+        data: JSON.stringify(email),
         beforeSend: function () {
           $("#send").prop("disabled", true);
           $("#send").text("Sending");
@@ -21,26 +16,22 @@ $(document).ready(function () {
         success: function (data) {
           $("#newsletter_feedback").html('<div class="alert alert-primary" role="alert">Thanks for signing up to our newsletter!</div>');
         },
-        error: function() {
+        error: function () {
           $("#newsletter_feedback").html('<div class="alert alert-danger" role="alert">An error ocurred, please try again!</div>');
+          $("#send").prop("disabled", false);
         }
        });
   });
-  $('#newsletter-sidebar').submit(function (event) {
-    event.preventDefault();
+   $('#newsletter-sidebar').submit(function (event) {
+        event.preventDefault();
 
-    var email = $("#email-sidebar").val();
+    var email = {'email': $("#email-sidebar").val()};
     $.ajax({
         type: "POST",
-        url: "https://api.elasticemail.com/v2/contact/add",
-        data: {
-          'publicAccountID': '7616a022-4e37-40ba-b90e-6c3735d8440e',
-          'email': email,
-          'listName': 'Uetke Newsletter',
-          'activationReturnUrl': 'https://www.uetke.com',
-          'activationTemplate': '',
-          'source': 'WebForm',
-          'ewf_captcha': 'false'},
+        dataType: 'json',
+        contentType: 'application/json',
+        url: "https://uetke.uetke.com/subscribe-newsletter",
+        data: JSON.stringify(email),
         beforeSend: function () {
           $("#send-sidebar").prop("disabled", true);
           $("#send-sidebar").text("Sending");
@@ -48,7 +39,7 @@ $(document).ready(function () {
         success: function (data) {
           $("#newsletter_feedback-sidebar").html('<div class="alert alert-primary" role="alert">Thanks for signing up to our newsletter!</div>');
         },
-        error: function() {
+        error: function () {
           $("#newsletter_feedback-sidebar").html('<div class="alert alert-danger" role="alert">An error ocurred, please try again!</div>');
           $("#send-sidebar").prop("disabled", false);
         }
